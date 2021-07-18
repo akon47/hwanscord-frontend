8a8df470c9587052b6a1a63676ab9c28c771f001
Kim Hwan <akon47@naver.com>
2021-07-18 09:28:40 +0900

    효과음 변경

--- src/audio/index.js
+++ src/audio/index.js
@@ -1,13 +1,13 @@
 function playVoiceChannelConnectionSound() {
   let audio = new Audio(
-    "https://discord.com/assets/5dd43c946894005258d85770f0d10cff.mp3"
+    "https://kimhwan.kr/assets/Wood_Plank_Flicks.mp3"
   );
   audio.play();
 }
 
 function playVoiceChannelDisconnectionSound() {
   let audio = new Audio(
-    "https://discord.com/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3"
+    "https://kimhwan.kr/assets/Metallic_Clank.mp3"
   );
   audio.play();
 }
