b8811d2b0dbe1bcc736e453e4193e527fdcebf23
Kim Hwan <akon47@naver.com>
2021-08-09 11:31:37 +0900

    로그인/회원가입 폼에 대한 테스트 케이스 추가

--- src/components/SigininForm.spec.js
+++ src/components/SigininForm.spec.js
@@ -16,4 +16,30 @@ describe("SigninForm.vue", () => {
     const button = wrapper.find(".btn");
     expect(button.element.disabled).toBeTruthy();
   });
+  test("username이 입력되지 않으면 로그인 버튼이 비활성화 된다.", () => {
+    const wrapper = mount(SigninForm, {
+      data() {
+        return {
+          username: "",
+          password: "test"
+        };
+      },
+      router
+    });
+    const button = wrapper.find(".btn");
+    expect(button.element.disabled).toBeTruthy();
+  });
+  test("password가 입력되지 않으면 로그인 버튼이 비활성화 된다.", () => {
+    const wrapper = mount(SigninForm, {
+      data() {
+        return {
+          username: "test",
+          password: ""
+        };
+      },
+      router
+    });
+    const button = wrapper.find(".btn");
+    expect(button.element.disabled).toBeTruthy();
+  });
 });
